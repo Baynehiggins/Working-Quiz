842f5c9a34eacca0f80ea23f0862d5c224661826
Baynehiggins <Bayne_higgins@biswashington.org>
2025-01-30 15:27:22 +0000

    yay

---
+++ quiz.py
@@ -0,0 +1,19 @@
+quiz_questions = [ 
+    "who was the first amerian president",
+    "what scientist discovered polonium",
+    "who was the first person to walk on the moon",
+    "when was the start of wwii",
+]
+quiz_answers = [
+    "george washington",
+    "marie curie",
+    "niel armstrong",
+    "September 1, 1939"
+]
+
+for 1 in range (2):
+    print(questions[1])
+    answer = input
+    if answer == answers[1]:
+        print("You are Correct!")
+        score == 1
